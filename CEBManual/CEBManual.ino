3b389451b3d31bda66d04752cec6a8cd4eef9e49
Zach Dwiel <zdwiel@plotwatt.com>
2012-08-11 16:12:21 -0400

    add code to toggle the shaker on and off

--- CEBManual/CEBManual.ino
+++ CEBManual/CEBManual.ino
@@ -15,11 +15,11 @@ const int secondary_right = 5;
 const int shaker          = 9;
 
 // button pins
-const int manual_down  = 18;
-const int manual_up    = 19;
-const int manual_left  = 20;
-const int manual_right = 21;
-// const int manual_shaker = xxx;
+const int manual_down   = 18;
+const int manual_up     = 19;
+const int manual_left   = 20;
+const int manual_right  = 21;
+const int manual_shaker = 13;
 
 void setup() {
   Serial.begin(9600);
@@ -33,10 +33,11 @@ void setup() {
   pinMode(secondary_right, OUTPUT);
   pinMode(shaker,          OUTPUT);
   
-  pinMode(manual_down,  INPUT_PULLUP);
-  pinMode(manual_up,    INPUT_PULLUP);
-  pinMode(manual_left,  INPUT_PULLUP);
-  pinMode(manual_right, INPUT_PULLUP);
+  pinMode(manual_down,   INPUT_PULLUP);
+  pinMode(manual_up,     INPUT_PULLUP);
+  pinMode(manual_left,   INPUT_PULLUP);
+  pinMode(manual_right,  INPUT_PULLUP);
+  pinMode(manual_shaker, INPUT_PULLUP);
     
   Serial.println("Initialization Complete");
 }
@@ -49,9 +50,14 @@ void manual_control(int output_pin, int input_pin) {
   }
 }
 
+// blinking helps know that the control board is powered
 const unsigned int blink_speed = 30000;
 void loop() {
+  unsigned int shaker_state = 0;
   unsigned int i = 0;
+  unsigned int shaker_timer = 0;
+  unsigned int last_shaker_toggle_state = 0;
+  
   while(1) {
     if(i < blink_speed) {
       digitalWrite(11, HIGH);
@@ -68,6 +74,26 @@ void loop() {
     digitalWrite(primary_up,      !digitalRead(manual_up));
     digitalWrite(secondary_left,  !digitalRead(manual_left));
     digitalWrite(secondary_right, !digitalRead(manual_right));
+    
+    // the shaker_timer is used to sample the toggle input to avoid 
+    // switch bouncing to oscilate the shaker state
+    if(shaker_timer > 10000) {
+      shaker_timer = 0;
+      
+      // sample the manual_shaker state
+      unsigned int current_shaker_toggle_state = !digitalRead(manual_shaker);
+      
+      // if the current state is on and the last state was off, toggle the
+      // state of the shaker output
+      if(current_shaker_toggle_state && !last_shaker_toggle_state) {
+        shaker_state = !shaker_state;
+        digitalWrite(shaker, shaker_state);
+      }
+      
+      last_shaker_toggle_state = current_shaker_toggle_state;
+    } else {
+      shaker_timer += 1;
+    }
   }
 }
 
